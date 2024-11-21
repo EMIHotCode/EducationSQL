94fb673d2e63826fb9832dbd7170f3cb3e443f8d
EMIHotCode <terminal7@dtn.ru>
2024-11-21 20:46:30 +0300

    some change

---
+++ ADO.NET/PhoneBook_v3/PhoneBook_v3.BL/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace PhoneBook_v3.BL;
+
+public class Contact
+{
+    public int? Id { get; set; }
+    
+    public required string Name { get; set; }  // required обязательно для заполнения
+    public string? LastName { get; set; }
+    public string? Patronymic { get; set; }
+    
+    public List<Phone> Phones { get; set; } = [];
+}
---
+++ ADO.NET/PhoneBook_v3/PhoneBook_v3.BL/Phone.cs
@@ -0,0 +1,9 @@
+﻿namespace PhoneBook_v3.BL;
+
+public class Phone
+{
+    public required string Number { get; set; }
+    public required string Type { get; set; }
+    public string? Comment { get; set; }
+}
+// и еще изменения 
